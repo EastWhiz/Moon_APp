076f57ad28793a6e6b96c4a2c5e369e774d28b5b
Abdullah Ayaz <atillawall@gmail.com>
2024-12-08 15:22:17 +0500

    comments removed and further development started

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,35 +1,82 @@
-import React, { Suspense, useEffect, useState } from "react";
-import * as THREE from "three";
 import { Box, Button, FormControlLabel, Grid2, styled, Switch, Tab, Tabs, TextField, Typography } from "@mui/material";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sphere, useTexture } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 
 const mainUrl = "https://phpstack-1376322-5078804.cloudwaysapps.com/images";
 
 const Moon = () => {
-    const textureURL = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/17271/lroc_color_poles_1k.jpg";
-    const displacementURL = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/17271/ldem_3_8bit.jpg";
 
-    // Use THREE's TextureLoader
-    const texture = new THREE.TextureLoader().load(textureURL);
-    const displacementMap = new THREE.TextureLoader().load(displacementURL);
+    const moonRef = useRef();
+
+    const textureURL = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/17271/lroc_color_poles_1k.jpg";
+
+    const [texture] = useTexture([textureURL]);
+
+    useFrame(() => {
+        if (moonRef.current) {
+            // moonRef.current.rotation.y += 0.005;
+            // moonRef.current.rotation.x += 0.005;
+        }
+    });
 
     return (
-        <mesh>
-            {/* Geometry */}
-            <sphereGeometry args={[5, 200, 200]} /> {/* Radius = 5, Segments = 200 */}
-            {/* Material */}
-            <meshStandardMaterial
-                map={texture}
-                displacementMap={displacementMap}
-                displacementScale={0.1}
-                bumpMap={displacementMap}
-                bumpScale={0.1}
-                roughness={1.0} // Matte surface
-                metalness={0.0} // Non-metallic surface
+        <>
+            <Sphere args={[2, 60, 60]} ref={moonRef} rotation={[0, 600, 0]}>
+                <meshPhongMaterial
+                    attach="material"
+                    color={0xffffff}
+                    map={texture}
+                    bumpScale={0.04}
+                    reflectivity={0}
+                    sshininess={0}
+                />
+            </Sphere>
+
+            {/* Lights */}
+            {/* <ambientLight intensity={0} color={0xffffff} /> */}
+
+            <directionalLight
                 color={0xffffff}
+                intensity={10}
+                // position={[100, -100, -240]}
+                // position={[100, -100, -210]}
+                // position={[100, -100, -170]}
+                // position={[100, -100, -130]}
+                // position={[100, -100, -90]}
+                // position={[100, -100, -50]}
+                // position={[100, -100, -10]}
+                position={[100, -100, 30]}
+            // position={[100, -100, 70]}
+            // position={[100, -100, 110]}
+            // position={[100, -100, 150]}
+            // position={[100, -100, 190]}
+            // position={[100, -100, 240]}
+            // position={[100, -100, 250]}
+            // position={[-100, -100, 250]}
+            // position={[-100, -100, 240]}
+            // position={[-100, -100, 190]}
+            // position={[-100, -100, 150]}
+            // position={[-100, -100, 110]}
+            // position={[-100, -100, 70]}
+            // position={[-100, -100, 30]}
+            // position={[-100, -100, -10]}
+            // position={[-100, -100, -50]}
+            // position={[-100, -100, -90]}
+            // position={[-100, -100, -130]}
+            // position={[-100, -100, -170]}
+            // position={[-100, -100, -210]}
+            // position={[-100, -100, -240]}
             />
-        </mesh>
+
+            <hemisphereLight
+                skyColor={new THREE.Color(0.5, 0.5, 0.5)}  // Lighter, more blue sky color
+                groundColor={new THREE.Color(0.5, 0.5, 0.5)}  // More saturated blue ground color
+                intensity={0.2}
+                position={[0, 0, 0]}
+            />
+        </>
     );
 };
 
@@ -99,15 +146,10 @@ const App = () => {
     };
 
     const [date, setDate] = useState();
-    // const [isDateOn, setIsDateOn] = useState();
 
     const handleDate = (event) => {
         setDate(event.target.value);
     };
-
-    // const handleIsDateOn = (event) => {
-    //   setIsDateOn(event.target.value);
-    // };
 
     const [city, setCity] = useState();
     const [isCityOn, setIsCityOn] = useState(true);
@@ -179,13 +221,10 @@ const App = () => {
                 flexDirection: { xs: 'column', sm: 'row', md: 'row' },
             }}
         >
-            {/* Left side */}
             <Box sx={{
-                //minHeight: { xs: '400px', sm:'400px', md: '600px' },
                 width: { xs: '100%', sm: '60%', md: '65%' },
                 overflow: 'hidden',
                 background: "url('wall2.jfif')",
-                // py: { xs: "240px", sm: "300px", md: "400px" },
                 backgroundRepeat: "no-repeat",
                 backgroundSize: 'cover',
                 display: 'flex',
@@ -201,17 +240,11 @@ const App = () => {
                         position: "absolute",
                         width: { xs: '50%', sm: '70%', md: '55%', lg: '35%', xl: '30%', },
                         minWidth: { xs: '250px' },
-                        // p: { xs: 2, sm: 3, md: 4 },
                         padding: { xs: '20px', sm: '20px', md: '25px', lg: '25px', xl: '25px' },
                         textAlign: "center",
-                        //fontFamily: "Arial, sans-serif",
                         boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
                         background: activeDesign && activeDesign.background,
                         color: activeDesign && activeDesign.color,
-                        //   cardBackground === 0 ? "black" :
-                        //     cardBackground === 4 ? "#d09281" :
-                        //       cardBackground === 5 ? "#718f91" :
-                        //         "white",
                         border: border === 0 ? "none" : "20px solid",
                         borderColor: border === 1 ? "#161715" : border === 2 ? "#e3e0de" : "none",
                         height: `${increaseBySixtyPercent(width)}px`
@@ -220,36 +253,17 @@ const App = () => {
                     <Box style={{ width: "100%", height: "100%", border: "1px solid silver" }}>
                         <Grid2 container direction="column" alignItems="center">
                             <Grid2 item>
-                                {/* <Box
-                  component="img"
-                  src={activeDesign && activeDesign.moon} // Replace with your image path
-                  alt="Moon"
-                  sx={{
-                    width: "100%",
-                    maxWidth: { xs: "120px", sm: "160px", md: "280px" },
-                    //height: "auto",
-                    borderRadius: "50%",
-                    my: { xs: 1, sm: 3, md: 4 },
-                  }}
-                /> */}
                                 <Canvas
-                                    camera={{ position: [0, 0, 10], fov: 75 }}
+                                    camera={{ position: [0, 0, 4], fov: 75 }}
                                     style={{ height: "320px", width: "100%" }}
                                 >
-                                    {/* Brighter Ambient Light */}
-                                    <ambientLight intensity={1.5} /> {/* Increased ambient light intensity */}
-
-                                    {/* Stronger Directional Light */}
-                                    <directionalLight intensity={2} position={[-10, 10, 10]} /> {/* Brighter directional light */}
-                                    <directionalLight intensity={1.5} position={[10, -10, -10]} /> {/* Additional light for better balance */}
-
-                                    {/* Moon */}
-                                    <Suspense fallback={null}>
-                                        <Moon />
-                                    </Suspense>
-
-                                    {/* OrbitControls */}
-                                    <OrbitControls enablePan={false} enableZoom={false} enableRotate={true} />
+                                    <OrbitControls
+                                        enablePan={false}
+                                        enableZoom={false}
+                                        enableRotate={false}
+                                        touches={{ ONE: THREE.TOUCH.ROTATE, TWO: THREE.TOUCH.DOLLY_ROTATE }}
+                                    />
+                                    <Moon />
                                 </Canvas>
                             </Grid2>
 
@@ -355,11 +369,7 @@ const App = () => {
                 </Box>
             </Box>
 
-            {/* Right side */}
             <Box sx={{ width: { xs: '100%', sm: '40%', md: '35%' }, pt: 0 }}>
-
-                {/* Customization Section */}
-
                 <Box >
                     <Tabs sx={{
                         border: "1px solid #ccc",
@@ -368,15 +378,6 @@ const App = () => {
                         onChange={handleChange}
                         textColor="black"
                         indicatorColor="none"
-
-                    // sx={{
-                    //   width: "100%",
-                    //   overflow: "unset",
-                    //   '& .MuiTabs-scroller': {
-                    //     //overflow: "unset !important",
-                    //     width: '100%',  // Active tab color
-                    //   },
-                    // }}
                     >
                         <Tab label="Design" sx={{
                             borderRight: '1px solid #ccc',
@@ -387,7 +388,6 @@ const App = () => {
                             '&.Mui-selected': { backgroundColor: '#e04848', color: 'white', },
                         }} />
                         <Tab label="Moment" sx={{
-                            // borderLeft: '1px solid #ccc',
                             borderRight: '1px solid #ccc',
                             width: "25%",
                             padding: { xs: "10px 0", sm: "16px 0", md: '16px 0' },
@@ -396,7 +396,6 @@ const App = () => {
                             '&.Mui-selected': { backgroundColor: '#e04848', color: 'white', },
                         }} />
                         <Tab label="Text" sx={{
-                            // borderLeft: '1px solid #ccc',
                             borderRight: '1px solid #ccc',
                             width: "25%",
                             padding: { xs: "10px 0", sm: "16px 0", md: '16px 0' },
@@ -415,7 +414,6 @@ const App = () => {
                         }} />
                     </Tabs>
 
-                    {/* Content Area for Each Tab */}
                     <Box sx={{ border: '' }}>
                         {value === 0 &&
                             <div>
@@ -431,21 +429,15 @@ const App = () => {
                                                     src={design.smMoon}
                                                     alt={`Image ${index + 1}`}
                                                     onClick={() => {
-                                                        //  console.log(index);
-                                                        // console.log(designs);
 
                                                         let temp = [...designs];
-                                                        // console.log(temp[index]);
                                                         designs.forEach((element, indexInside) => {
                                                             if (indexInside === index) {
                                                                 temp[indexInside] = { ...temp[indexInside], active: true };
-                                                                // console.log(indexInside);
                                                             } else {
                                                                 temp[indexInside] = { ...temp[indexInside], active: false };
-                                                                // console.log(indexInside);
                                                             }
                                                         });
-                                                        // console.log(temp);
                                                         setDesigns(temp);
 
                                                     }}
@@ -483,8 +475,7 @@ const App = () => {
                                                         p: "2px",
                                                         borderRadius: "10px",
                                                         border: "3px solid #cccccc",
-                                                        borderColor: index === 0 ? "#f76916" : "f76916", // Highlight the first image
-                                                        //backgroundColor: index === 0 ? "black" : "transparent", // Black background for the first box
+                                                        borderColor: index === 0 ? "#f76916" : "f76916",
                                                         cursor: "pointer",
                                                     }}
                                                 />
@@ -535,7 +526,6 @@ const App = () => {
                                             mb: 3
                                         }} />
 
-                                    {/* Time and Date */}
                                     <Typography variant="h6" sx={{ fontSize: { xs: "14px", md: "16px" }, mb: 1 }}>
                                         Determine the time
                                         <Box component="span" sx={{ mx: 2 }}>
@@ -555,7 +545,6 @@ const App = () => {
                                             mb: 3
                                         }} />
 
-                                    {/* Price Section */}
                                     <Typography variant="body2" sx={{
                                         my: 3, borderTop: "2px solid #474665",
                                         borderBottom: "2px solid #474665", color: "#474665", py: "6px", textAlign: "center",
@@ -579,7 +568,6 @@ const App = () => {
                         {value === 2 &&
                             <div>
                                 <Box sx={{ p: 2, }}>
-                                    {/* Title Input */}
                                     <Typography variant="h6" sx={{ fontSize: { xs: "14px", md: "16px" }, mb: 1 }}>
                                         Add a title (optional)
                                     </Typography>
@@ -587,11 +575,9 @@ const App = () => {
                                         fullWidth id=""
                                         value={title}
                                         onChange={handleTitle}
-                                        //defaultValue="My Moonshine"
                                         placeholder="My Moonshine"
                                         variant="standard" sx={{ mb: 3 }} />
 
-                                    {/* Personal Text Input */}
                                     <Typography variant="h6" sx={{ fontSize: { xs: "14px", md: "16px" }, mb: 1 }}>
                                         Write a personal text (optional)
                                     </Typography>
@@ -600,7 +586,6 @@ const App = () => {
                                         onChange={handlePrText}
                                         variant="standard" sx={{ mb: 3, }} />
 
-                                    {/* Price Section */}
                                     <Typography variant="body2" sx={{
                                         my: 3, borderTop: "2px solid #474665",
                                         borderBottom: "2px solid #474665", color: "#474665", py: "6px", textAlign: "center",
@@ -642,7 +627,6 @@ const App = () => {
                                                         height: 46,
                                                         p: "2px",
                                                         borderRadius: "10px",
-                                                        // border: "3px solid #cccccc",
                                                         border: index === border ? "3px solid #f76916" : "3px solid #cccccc", // Highlight the first image
                                                         cursor: "pointer",
                                                     }}
@@ -660,7 +644,6 @@ const App = () => {
                                         id=""
                                         placeholder="E-Mail Adresse (optional)"
                                         variant="standard" sx={{ mb: 3, }} />
-                                    {/* Price Section */}
                                     <Typography variant="body2" sx={{
                                         my: 3, borderTop: "2px solid #474665",
                                         borderBottom: "2px solid #474665", color: "#474665", py: "6px", textAlign: "center",
