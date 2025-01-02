457e23d25ab4941dabff8676342c12dcf2b55761
Abdullah Ayaz <atillawall@gmail.com>
2025-01-03 02:15:23 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::get('/astronomy-api/appearance', [ApiController::class, 'getMoonAppearance'])->name('moonAppearance');
 Route::get('/astronomy-api/moon', [ApiController::class, 'getMoonPicture'])->name('moonAppearance');
 Route::get('/geo-names', [ApiController::class, 'getGeoNames'])->name('moonAppearance');
-Route::get('/download-font/{fontName}', function ($fontName) {
+Route::get('/fonts/{fontName}', function ($fontName) {
     $path = public_path('fonts/' . $fontName);
 
     if (file_exists($path)) {
