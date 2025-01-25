667e8e11a4aca77074ea0cd3fdc24ab03dfe6dc1
Abdullah Ayaz <atillawall@gmail.com>
2025-01-25 13:54:07 +0500

    php limits increased

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,10 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 Route::get("/function", function () {
     // Browsershot::url('https://www.google.com')->save('ss.png');
 
+    ini_set('max_execution_time', 600);       // 600 seconds = 10 minutes
+    ini_set('upload_max_filesize', '1024M'); // 1024 MB
+    ini_set('post_max_size', '1024M');
+
     Browsershot::url('http://localhost:3000/?design=midnight_blue&cityVisible=true&dateVisible=true&starsEffect=true&title=Lahore&titleFont=outfit&paragraphText=City%20of%20Lights&paragraphTextFont=italiana&selectedDate=12-01-2025&city={%22name%22:%22Lahore,%20Punjab,%20Pakistan%22,%22value%22:%22Lahore%22,%22lat%22:%2231.558%22,%22lng%22:%2274.35071%22}&titleFontSize=1&paragraphFontSize=0.75&moon=full&rotateValue=45&newMoon=false')
         ->waitUntilNetworkIdle()
         ->timeout(180000)
