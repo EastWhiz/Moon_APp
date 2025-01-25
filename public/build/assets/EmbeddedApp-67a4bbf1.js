01caf10bdc9869b5d7ccdcf59421aba789965c31
Abdullah Ayaz <atillawall@gmail.com>
2025-01-25 16:32:55 +0500

    new build added

---
+++ public/build/assets/EmbeddedApp-67a4bbf1.js
@@ -0,0 +1,1 @@
+import{q as d,j as e}from"./app-adbb6c1a.js";const r=()=>(d().props,e.jsx("div",{children:"Embedded App"}));export{r as default};
---
+++ public/build/assets/useTheme-cca496f1.js
@@ -0,0 +1,1 @@
+import"./app-adbb6c1a.js";import{n as t,t as m,L as s}from"./Box-a30c2b2a.js";function u(){const e=t(s);return e[m]||e}export{u};
