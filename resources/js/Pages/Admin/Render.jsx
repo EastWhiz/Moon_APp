3bd061d0087eda78a05776629592773c3b31a0d8
Abdullah Ayaz <atillawall@gmail.com>
2025-02-02 00:50:10 +0500

    changes

--- resources/js/Pages/Admin/Render.jsx
+++ resources/js/Pages/Admin/Render.jsx
@@ -440,7 +440,7 @@ const App = () => {
                     result.data.table.rows[0].cells[0],
                     parseFloat(city.lat),
                     parseFloat(city.lng),
-                    new Date(format(selectedDate, "MM-dd-yyyy"))
+                    new Date(format(selectedDate, "yyyy-MM-dd") + 'T00:00:00')
                 )
                 setRotateValue(parseFloat(functionResponse.rotationAngle));
             } catch (error) {
